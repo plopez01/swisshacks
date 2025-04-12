fb6a87cea762a89c04556a5680a64e5de1c349ee
Anouar El Haddadi El Yahyati <anouar.el.haddadi@estudiantat.upc.edu>
2025-04-12 16:36:40 +0000

    Adding the docx

---
+++ description_extracter.py
@@ -0,0 +1,19 @@
+import base64
+
+
+
+def extract_docx_text_from_base64(description_value):
+    try:
+        # Step 1: Decode the Base64 string
+        description_bytes = base64.b64decode(description_value, validate=True)
+
+        # Step 2: Write the bytes to a .txt file
+        with open("description.txt", "wb") as f:
+            f.write(description_bytes)
+
+        print("✅ File written successfully to description.txt")
+
+    except Exception as e:
+        print(f"❌ Error: {e}")
+        return None
+
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from ConsistencyModel import *
 
 import passport_reader
 import pdf_decoder
+from description_extracter import extract_docx_text_from_base64
+from docx_extracter import docx_extracter
 
 import api
 
@@ -20,6 +22,7 @@ status = "active"
 while status != "gameover":
     try:
         passport_reader.read_passport(cm, gamedata['client_data']['passport'])
+        docx_extracter(cm, gamedata['client_data']['profile'])
 
         gamedata = api.submit_decision(cm.inconsistencies == 0, session, gamedata['client_id'])
         

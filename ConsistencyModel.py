a67c74a7ae35b3bce5959d5b539f7a65ca114187
plopez01 <plopez.01.info@gmail.com>
2025-04-12 14:52:45 +0200

    Work on main, add new model

--- ConsistencyModel.py
+++ ConsistencyModel.py
@@ -1,53 +1,64 @@
 from ConsistencyFields import *
 
 class ConsistencyModel:
-    def __init__(self, handler):
-        self.name = ConsistencyField("name", handler)
-        self.surname_1 = ConsistencyField("surname_1", handler)
-        self.surname_2 = ConsistencyField("surname_2", handler)
+    def __init__(self, external_handler):
+        self._external_handler = external_handler
 
-        self.sex = ConsistencyField("sex", handler)
+        self.name = ConsistencyField("name", self._handler)
+        self.surname_1 = ConsistencyField("surname_1", self._handler)
+        self.surname_2 = ConsistencyField("surname_2", self._handler)
+
+        self.sex = ConsistencyField("sex", self._handler)
 
         # This needed?
-        self.id_type = ConsistencyField("id_type", handler)
+        self.id_type = ConsistencyField("id_type", self._handler)
 
-        self.passport_num = ConsistencyField("passport_num", handler)
-        self.passport_issue_date = DateField("passport_issue_date", handler)
-        self.passport_expiry_date = DateField("passport_expiry_date", handler)
+        self.passport_num = ConsistencyField("passport_num", self._handler)
+        self.passport_issue_date = DateField("passport_issue_date", self._handler)
+        self.passport_expiry_date = DateField("passport_expiry_date", self._handler)
 
         # Check if this is actually needed
-        self.passport_ocr = ConsistencyField("passport_ocr", handler)
+        self.passport_ocr = ConsistencyField("passport_ocr", self._handler)
 
-        self.birth_date = DateField("birth_date", handler)
+        self.birth_date = DateField("birth_date", self._handler)
 
-        self.country = ConsistencyField("country", handler)
-        self.city = ConsistencyField("city", handler)
+        self.country = ConsistencyField("country", self._handler)
+        self.city = ConsistencyField("city", self._handler)
 
-        self.phone_num = ConsistencyField("phone_num", handler)
-        self.email = ConsistencyField("email", handler)
+        self.phone_num = ConsistencyField("phone_num", self._handler)
+        self.email = ConsistencyField("email", self._handler)
 
-        self.currency = ConsistencyField("currency", handler)
-        self.building_num = ConsistencyField("building_num", handler)
-        self.street_name = ConsistencyField("street_name", handler)
-        self.postal_code = ConsistencyField("postal_code", handler)
+        self.currency = ConsistencyField("currency", self._handler)
+        self.building_num = ConsistencyField("building_num", self._handler)
+        self.street_name = ConsistencyField("street_name", self._handler)
+        self.postal_code = ConsistencyField("postal_code", self._handler)
 
         # Personal info
-        self.marital_status = ConsistencyField("marital_status", handler)
+        self.marital_status = ConsistencyField("marital_status", self._handler)
 
         # Education
         # TODO: this should maybe be broken into separate fields
-        self.education = ConsistencyField("education", handler)
+        self.education = ConsistencyField("education", self._handler)
 
         # Employment
         # TODO: this should maybe be broken into separate fields
-        self.employment = ConsistencyField("employment", handler)
+        self.employment = ConsistencyField("employment", self._handler)
 
-        self.wealth = ConsistencyField("wealth", handler)
+        self.wealth = ConsistencyField("wealth", self._handler)
 
         # TODO: this should maybe be broken into separate fields
-        self.bank_account_info = ConsistencyField("bank_account_info", handler)
+        self.bank_account_info = ConsistencyField("bank_account_info", self._handler)
 
-        self.signature = ConsistencyField("signature", handler)
+        self.signature = ConsistencyField("signature", self._handler)
 
+    def _handler(self, field, reason):
+        self._external_handler(field, reason)
 
-   
+class InconsistencyCounterModel(ConsistencyModel):
+    def __init__(self, external_handler):
+        super().__init__(external_handler)
+        self.inconsistencies = 0
+         
+    def _handler(self, field, reason):
+        self.inconsistencies += 1
+        self._external_handler(field, reason)
